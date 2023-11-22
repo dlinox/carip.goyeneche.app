7c7e013fa9f080769d7662c6ac6d3289595e2404
lino <nearlino20@gmail.com>
2023-11-22 06:16:28 -0500

    pasda

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/assets/styles/scss/app.scss",
                 "resources/js/app.js",
             ],
             refresh: true,
